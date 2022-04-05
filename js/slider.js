94a2002e045dc166e917a819f7477e809cfe2974
qmtazmir <96815202+qmtazmir@users.noreply.github.com>
2022-04-05 16:34:30 +0600

    sliderdone

---
+++ js/slider.js
@@ -0,0 +1,48 @@
+const images = [
+  "images/p-1.jpg",
+  "images/p-2.jpg",
+  "images/p-3.jpg",
+  "images/p-4.jpg",
+  "images/p-5.jpg",
+  "images/p-6.jpg",
+  "images/p-7.jpg",
+  "images/p-8.jpg",
+  "images/p-9.jpg",
+  "images/p-10.jpg",
+  "images/p-11.jpg",
+  "images/p-12.jpg",
+  "images/p-13.jpg",
+  "images/p-14.jpg",
+  "images/p-15.jpg",
+  "images/p-16.jpg",
+  "images/p-17.jpg",
+  "images/p-18.jpg",
+  "images/p-19.jpg",
+  "images/p-20.jpg",
+];
+
+let imgIndex = 0;
+
+const imgElement = document.getElementById("sliderImages");
+
+setInterval(() => {
+  // console.log(imgIndex);
+  if (imgIndex >= images.length) {
+    imgIndex = 0;
+  }
+  const imgUrl = images[imgIndex];
+  //   console.log(imgUrl);
+  imgElement.setAttribute("src", imgUrl);
+
+  imgIndex++;
+}, 1000);
+
+// [images/p-12.jpg,
+// images/p-13.jpg,
+// images/p-14.jpg,
+// images/p-15.jpg,
+// images/p-16.jpg,
+// images/p-17.jpg,
+// images/p-18.jpg,
+// images/p-19.jpg,
+// images/p-20.jpg,]
